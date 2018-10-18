48e580ab8bed5821ae13ca8d82494ce9f9041770
jomanazene28 <jomana.zene1999@gmail.com>
2018-10-18 12:20:06 +0300

    sign up

--- app/src/main/java/om/banat/karawan/karawantaskmngr2018/SignInActivity.java
+++ app/src/main/java/om/banat/karawan/karawantaskmngr2018/SignInActivity.java
@@ -1,48 +1,115 @@
 package om.banat.karawan.karawantaskmngr2018;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
-    //private EditText etfirstname;
-    // private EditText etlastname;
-    // private  EditText etphone;
-    //  private  EditText etemail;
-    // private EditText etpassword;
+    private EditText etfirstname;
+     private EditText etlastname;
+     private  EditText etphone;
+      private  EditText etemail;
+    private EditText etpassword;
     //1.
     private FirebaseAuth auth;
     private FirebaseUser user;
+    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
-        //  etfirstname=(EditText)findViewById(R.id.etfirstname);
-        // etlastname=(EditText)findViewById(R.id.etlastname);
-        //  etphone=(EditText)findViewById(R.id.etphone);
-        //  etemail=(EditText)findViewById(R.id.etemail);
-        //  etpassword=(EditText)findViewById(R.id.etPassword);
+        etfirstname=(EditText)findViewById(R.id.etfirstname);
+        etlastname=(EditText)findViewById(R.id.etlastname);
+        etphone=(EditText)findViewById(R.id.etphone);
+        etemail=(EditText)findViewById(R.id.etemail);
+        etpassword=(EditText)findViewById(R.id.etPassword);
         auth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser = auth.getCurrentUser();
-        if (firebaseUser == null) {
-            startActivity(new Intent(this, SignInActivity.class));
-            finish();
-            return;
-        } else {
-            String userName = firebaseUser.getDisplayName();
-            if (firebaseUser.getPhotoUrl() != null) {
-                String photoUrl = firebaseUser.getPhotoUrl().toString();
+        user=auth.getCurrentUser();
+        FirebaseUser firebaseUser;
+        firebaseUser = auth.getCurrentUser();
+        btnSave=(Button) this.<View>findViewById(R.id.btnSave);
+btnSave.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+    }
+});
+     dataHandler();
+
+        }
+
+    private void dataHandler() {
+        boolean isok = true;
+        String email = etemail.getText().toString();
+        String password = etpassword.getText().toString();
+        String firstName = etfirstname.getText().toString();
+        String lastName = etlastname.getText().toString();
+        String phone = etphone.getText().toString();
+
+        if (email.length() < 4 || email.indexOf('@')<0 || email.indexOf('.')<0)
+        {
+            etemail.setError("wrong Email");
+            {
+                isok = false;
+
+            }
+            if (password.length() < 8) {
+                etpassword.setError("Have to be least 8 char");
+                isok = false;
+            }
+            if (isok) {
+
+
             }
         }
-        FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+
+        else
+            etemail.setError("wrong Email");
+            {
+                isok = false;
+
+            }
+            if (password.length() < 8) {
+                etpassword.setError("Have to be least 8 char");
+                isok = false;
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //* creat firebase user using email password
+
+
+
+
+    FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
 
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -58,14 +125,38 @@ public class SignInActivity extends AppCompatActivity {
 
 
                 }
+                
             }
         };
 
 
 
-        }
+    private void creatAcount;(String email,String
 
+    public EditText getEtpassword() {
+        return etpassword;
+    ) {
+        String password;
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(SignInActivity.this, "Authentication Successful.", Toast.LENGTH_SHORT).show();
+                    //updateUserProfile(task.getResult().getUser());
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(SignInActivity.this, "Authentication failed."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
     }
+
+
+}
 
 
 
