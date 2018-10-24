f9c2018c827b26ac1eb698db4c461ba7a5625f95
jomanazene28 <jomana.zene1999@gmail.com>
2018-10-24 08:56:23 +0300

    aa

--- app/src/main/java/om/banat/karawan/karawantaskmngr2018/AddTaskActivity.java
+++ app/src/main/java/om/banat/karawan/karawantaskmngr2018/AddTaskActivity.java
@@ -2,12 +2,50 @@ package om.banat.karawan.karawantaskmngr2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class AddTaskActivity extends AppCompatActivity {
-
+    private EditText edTitle,edText,etDuetDate;
+    private SeekBar skbrImportant,skbrNecceary;
+    private TextView tvImportant,tvNecceary;
+    private Button btnSave,btnDatePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        edTitle=findViewById(R.id.edTitle);
+        edText=findViewById(R.id.edText);
+        etDuetDate=findViewById(R.id.etDuetDate);
+        skbrImportant=findViewById(R.id.skbrImportant);
+        skbrNecceary=findViewById(R.id.skbrNecceary);
+        tvImportant=findViewById(R.id.tvImportant);
+        tvNecceary=findViewById(R.id.tvNecceary);
+        btnSave=findViewById(R.id.btnSave);
+        btnDatePicker=findViewById(R.id.btnDatePicker);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnDatePicker.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+
+        });
+
+
+
+
+
+
+
+
     }
 }
