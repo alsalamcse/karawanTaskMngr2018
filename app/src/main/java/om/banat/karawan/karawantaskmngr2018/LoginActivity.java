4d9ce386b8a689e3fcc9671d8bc6dacad4361023
jomanazene28 <jomana.zene1999@gmail.com>
2018-11-06 13:21:06 +0200

    aa

--- app/src/main/java/om/banat/karawan/karawantaskmngr2018/LoginActivity.java
+++ app/src/main/java/om/banat/karawan/karawantaskmngr2018/LoginActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.security.PublicKey;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,25 +23,52 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        etEmail=(EditText)findViewById(R.id.etEmail);
-        etPassword=(EditText)findViewById(R.id. etPassword);
-        btnSignIn=(Button)findViewById(R.id.btnSignIn);
-        btnSignUp=(Button)findViewById(R.id.btnSignUp);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dataHandler();
             }
-        });
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),SignInActivity.class);
-                startActivity(i);
+
+            private void dataHandler() {
+
             }
 
         });
 
     }
 
-}
+    private void dataHandler() {
+        boolean isok = true;
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        if (email.length() < 4 || email.indexOf('@') < 0 || email.indexOf('.') < 0) {
+            etEmail.setError("wrong Email");
+            isok = false;
+
+        }
+    }
+    private void signIn(String email,String password){
+        FirebaseAuth auth;
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
